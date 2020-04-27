1bb4af23c0dff217c5e7ed7e7c757ef435e55429
pk10134 <62251449+pk10134@users.noreply.github.com>
2020-04-27 16:18:02 +0200

    Add files via upload

---
+++ server.R
@@ -0,0 +1,9 @@
+#serwer
+library(shiny)
+library(ggplot2)
+library(coronavirus)
+wybrane=coronavirus
+shinyServer(function(input, output) {
+ 
+})
+
---
+++ ui.R
@@ -0,0 +1,31 @@
+#ui
+library(shiny)
+library(plotly)
+library(coronavirus)
+
+
+shinyUI(fluidPage(
+  headerPanel("Analiza danych- koronawirus"),
+  
+  sidebarPanel(
+    selectInput(inputId = "Kraj",
+                label = "Wybierz kraj",
+                choices = unique(coronavirus$Country.Region),
+                selected = "Poland")
+  ),
+    
+    sidebarPanel(
+      selectInput(inputId = "Region",
+                  label = "Wybierz Region/Prowincję/Stan",
+                  choices = unique(coronavirus$Province.State))
+  ),
+  
+  sidebarPanel(
+    p("Wybierz rodzaj danych: "),
+    checkboxInput("confirmed", "Potwierdzone przypadki", FALSE),
+    checkboxInput("death", "Zmarli", FALSE),
+    checkboxInput("recovered", "Ozdrowiali", FALSE),
+    checkboxInput("Daily.change.in.total.tests", "Ilość testów", FALSE)),
+
+
+))
